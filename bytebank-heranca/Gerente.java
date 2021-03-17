04fc59935ec6f976be0d9b25280d8b5d3a1a315f
jorgefranca <jorgeartur.f@gmail.com>
2021-03-17 16:20:42 -0300

    modificado o metodo getBonificacao(). Usa do 'super.' para vincular 'bonificacao' e 'salario' à classe Funcionario (classe mãe)

--- bytebank-heranca/Gerente.java
+++ bytebank-heranca/Gerente.java
@@ -3,10 +3,12 @@ public class Gerente extends Funcionario{
 
     private int senha;
 
-    //reescrita do metodo
+    //reescrita do metodo: 
     public double getBonificacao(){
-        return super.salario;
+        return super.getSalario() + super.getBonificacao();
         //super indica que o atributo pertence a classe mae (Funcionario)
+        //super.getSalario() -> pega o salario da classe mae(classe Funcionario)
+        //de forma, podemos deixar os atributos da classe Funcionarios(classe mae) privados.
     }
 
     public void setSenha(int senha){
